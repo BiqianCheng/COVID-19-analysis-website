42d495e2a4f15b73ea11ff15c17c2e4fd53dc6c9
KatelynCheng <chengbiqian@foxmail.com>
2020-10-22 01:35:54 -0700

    add country, age, and sex on homepage, merge conflict, Fixes #5

--- web/pages/analytics/index.js
+++ web/pages/analytics/index.js
@@ -3,25 +3,59 @@ import Navbar from '../../components/Navbar/Navbar'
 import Button from '@material-ui/core/Button';
 import { useState } from 'react';
 import axios from 'axios'
+import { CircularProgress } from '@material-ui/core'
+import { Doughnut, Bar } from 'react-chartjs-2';
 
 export default function Analytics() {
 
+  const [data, setData] = useState()
+  const [loading, setLoading] = useState(false)
+
+  // var chartData = {
+  //   labels: ['Country', 'Age', 'Gender'],
+  //   datasets: [{
+  //     label: 'Covid Cases',
+  //     data: [1, 2, 3],
+  //     backgroundColor: [
+  //       'rgba(255, 99, 132, 0.2)',
+  //       'rgba(54, 162, 235, 0.2)',
+  //       'rgba(255, 206, 86, 0.2)',
+  //       'rgba(75, 192, 192, 0.2)',
+  //       'rgba(153, 102, 255, 0.2)',
+  //       'rgba(255, 159, 64, 0.2)'
+  //     ],
+  //     borderColor: [
+  //       'rgba(255, 99, 132, 1)',
+  //       'rgba(54, 162, 235, 1)',
+  //       'rgba(255, 206, 86, 1)',
+  //       'rgba(75, 192, 192, 1)',
+  //       'rgba(153, 102, 255, 1)',
+  //       'rgba(255, 159, 64, 1)'
+  //     ],
+  //     borderWidth: 1
+  //   }]
+  // }
+
   const testQueryServer = () => {
-    const testData = {
+    const testInputs = {
       "country": "China",
       "age": "36",
       "gender": "male"
     }
-
+    setLoading(true)
     axios.get(`/analytics/search/`, {
       params: {
-        data: testData,
+        data: testInputs,
       }
     })
       .then(({ data }) => {
-        console.log("Successfully talked to the server!: ", data)
+        console.log("Successfully talked to the server!: ", data.filteredData)
+        setData(data.filteredData)
       }).catch((error) => {
         console.log(error)
+      })
+      .finally(() => {
+        setLoading(false)
       })
   }
 
@@ -37,6 +71,43 @@ export default function Analytics() {
         <div className={styles.contentWrapper}>
           <Button variant="contained" color="primary" onClick={testQueryServer}>Test Query Server</Button>
         </div>
+        {loading &&
+          <div className={styles.loading}>
+            <CircularProgress style={{ color: 'black' }} size={16} />
+          </div>
+        }
+
+        {data &&
+          <>
+            <div className={styles.table}>
+              <div id="column-headers" className={styles.row}>
+                <div className={styles.column}>Index</div>
+                <div className={styles.column}>Reporting Date</div>
+                <div className={styles.column}>Country</div>
+                <div className={styles.column}>Location</div>
+                <div className={styles.column}>Age</div>
+                <div className={styles.column}>Gender</div>
+                <div className={styles.column}>Death</div>
+              </div>
+              {
+                data.map((point, index) =>
+                  <div className={styles.row} key={point.link}>
+                    <div className={styles.column}>{index + 1}</div>
+                    <div className={styles.column}>{point["reporting date"]}</div>
+                    <div className={styles.column}>{point.country}</div>
+                    <div className={styles.column}>{point.location}</div>
+                    <div className={styles.column}>{point.age}</div>
+                    <div className={styles.column}>{point.gender}</div>
+                    <div className={styles.column}>{point.death ? "Yes" : "No"}</div>
+                  </div>
+                )
+              }
+            </div>
+            {/* <div className={styles.charts}>
+              <Bar data={chartData} />
+            </div> */}
+          </>
+        }
       </div>
     </>
   )
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,22 +1,96 @@
-import styles from '../styles/pages/Home.module.css'
-import Navbar from '../components/Navbar/Navbar'
-import { useState } from 'react';
-import axios from 'axios'
+import styles from "../styles/pages/Home.module.css";
+import Navbar from "../components/Navbar/Navbar";
+import { useState } from "react";
+import axios from "axios";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+
+const countries = ["The United States", "Brazil", "India", "Russia", "China"];
+const age = ["18","23","24"];
+const sex = ["Male", "Female", "Other"];
 
 export default function Home() {
+  const [value, setValue] = React.useState({
+    countries: countries[0],
+    sex: sex[0],
+  });
+  const [inputValue, setInputValue] = React.useState({
+    countries: "",
+    sex: "",
+  });
   return (
     <>
       <Navbar />
       <div className={styles.pageContainer}>
         <div className={styles.header}>
-          <div className={styles.title}>
-            BBCDS Corona Analytics
-          </div>
+          <div className={styles.title}>BBCDS Covid-19 Analytics</div>
           <div className={styles.description}>
-            Helping you understand the current state of Corona
+            Helping you understand the current state of Covid-19
           </div>
+          <Container maxWidth="lg">
+            <Grid container spacing={5}>
+              {/* Country Autocompete */}
+              <Grid item xs={4}>
+                <Autocomplete
+                  value={value.countries}
+                  onChange={(event, newValue) => {
+                    setValue({ ...value,countries: newValue });
+                  }}
+                  // inputValue={inputValue.countries}
+                  // onInputChange={(event, newInputValue) => {
+                  //   setInputValue({ countries: newInputValue });
+                  // }}
+                  id="controllable-states-demo"
+                  options={countries}
+                  style={{ width: 300 }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Country" variant="outlined" />
+                  )}
+                />
+              </Grid>
+
+              {/* Age selection */}
+              <Grid item xs={4}>
+                <Autocomplete
+                  value={value.age}
+                  onChange={(event, newValue) => {
+                    setValue({...value,age: newValue});
+                  }}
+                  id="controllable-states-demo"
+                  options={age}
+                  style={{ width: 300 }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Age" variant="outlined" />
+                  )}
+                />
+              </Grid>
+
+              {/* Sex autocomplete */}
+              <Grid item xs={4}>
+                <Autocomplete
+                  value={value.sex}
+                  onChange={(event, newValue) => {
+                    setValue({ ...value,sex: newValue });
+                  }}
+                  // inputValue={inputValue.sex}
+                  // onInputChange={(event, newInputValue) => {
+                  //   setInputValue({ ...inputValue, sex: newInputValue });
+                  // }}
+                  id="controllable-states-demo"
+                  options={sex}
+                  style={{ width: 300 }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Sex" variant="outlined" />
+                  )}
+                />
+              </Grid>
+            </Grid>
+          </Container>
         </div>
       </div>
     </>
-  )
+  );
 }
