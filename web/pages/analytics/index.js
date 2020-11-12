c6afbce0b2aaac8db231649d6707c0ea32d1a111
Dominic L <spruxgaming@gmail.com>
2020-11-11 22:23:05 -0800

    cleanup the page

--- web/pages/analytics/index.js
+++ web/pages/analytics/index.js
@@ -13,88 +13,6 @@ export default function Analytics() {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(false)
 
-  const testQueryServer = () => {
-    const testInputs = {
-      "country": "USA",
-      // "age": "53",
-      // "gender": "male"
-    }
-    setLoading(true)
-    axios.get(`/analytics/search/`, {
-      params: {
-        data: testInputs,
-        startDate: new Date('2020-01-02'),
-        endDate: new Date('2020-05-30')
-      }
-    })
-      .then(({ data }) => {
-        console.log("Successfully talked to the server!: ", data.filteredData)
-        setData(data.filteredData)
-      }).catch((error) => {
-        console.log(error)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }
-
-  const testInsertData = () => {
-    const jsonData = {
-      age: 33,
-      country: "USA",
-      location: "San Francisco",
-      gender: "male",
-      recovered: true,
-      death: false
-    }
-
-    axios.post(`/admin/insert/`, { jsonData })
-      .then(({ data }) => {
-        console.log("Succesfully inserted data into file: ", data.csv)
-      }).catch((error) => {
-        console.log(error)
-      })
-  }
-
-  const testUpdateData = () => {
-    let id = 1088
-
-    const jsonData = {
-      age: 54,
-      country: "USA",
-      location: "Los Angeles",
-      gender: "female",
-      recovered: true,
-      death: false
-    }
-
-    axios.put(`/admin/update/${id}`, { jsonData })
-      .then(({ data }) => {
-        console.log("Succesfully updated data in the dataset: ", data.updatedData)
-      }).catch((error) => {
-        if (error.response) {
-          console.log(error.response.data)
-        } else {
-          console.log(error)
-        }
-      })
-  }
-
-  const testDeleteData = () => {
-    let id = 1089
-
-    axios.delete(`/admin/delete/${id}`)
-      .then(() => {
-        console.log("Succesfully deleted data in the dataset: ", id)
-      }).catch((error) => {
-        if (error.response) {
-          console.log(error.response.data)
-        } else {
-          console.log(error)
-        }
-      })
-  }
-
   return (
     <>
       <Navbar />
@@ -104,28 +22,6 @@ export default function Analytics() {
             Analytics
           </div>
         </div>
-        <div className={styles.contentWrapper}>
-          <Button variant="contained" color="primary" onClick={testQueryServer}>Test Query Server</Button>
-          <Button variant="contained" color="primary" onClick={testInsertData}>Test Insert Data</Button>
-          <Button variant="contained" color="primary" onClick={testUpdateData}>Test Update Data</Button>
-          <Button variant="contained" color="primary" onClick={testDeleteData}>Test Delete Data</Button>
-        </div>
-        {loading &&
-          <div className={styles.loading}>
-            <CircularProgress style={{ color: 'black' }} size={16} />
-          </div>
-        }
-        {data &&
-          <div className={styles.analytics}>
-            <div className={styles.table}>
-              <DataTable data={data} key={data} />
-            </div>
-            <div className={styles.charts}>
-              <LocationsChart data={data} />
-              <AgeChart data={data} />
-            </div>
-          </div>
-        }
       </div>
     </>
   )
